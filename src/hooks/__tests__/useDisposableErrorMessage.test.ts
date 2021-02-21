9d1a182cee4bce2e00658156490cbb04850fc634
Yannick Fricke <YannickFricke@users.noreply.github.com>
2021-02-21 21:59:30 +0000

    Added jest support
    Added jest and ts-jest as dependencies. Also added the basic test for
    the useDisposableErrorMessage hook.

---
+++ src/hooks/__tests__/useDisposableErrorMessage.test.ts
@@ -0,0 +1,7 @@
+import { useDisposableErrorMessage } from '../useDisposableErrorMessage';
+
+describe('useDisposableErrorMessage', () => {
+    it('should be defined', () => {
+        expect(useDisposableErrorMessage).toBeDefined();
+    });
+});
