ad46ec7a848acf87cb37e6714abbd1bc275859b5
Yannick Fricke <YannickFricke@users.noreply.github.com>
2021-02-21 22:32:51 +0000

    Wrote all tests for the useDisposableErrorMessage hook

--- src/hooks/__tests__/useDisposableErrorMessage.test.ts
+++ src/hooks/__tests__/useDisposableErrorMessage.test.ts
@@ -1,7 +1,69 @@
+import { act, renderHook } from '@testing-library/react-hooks';
 import { useDisposableErrorMessage } from '../useDisposableErrorMessage';
 
+jest.useFakeTimers();
+
 describe('useDisposableErrorMessage', () => {
+    const newErrorMessage = 'i am an error';
+
     it('should be defined', () => {
         expect(useDisposableErrorMessage).toBeDefined();
+    });
+
+    it('should not have a error message by default', () => {
+        const { result } = renderHook(() => useDisposableErrorMessage());
+
+        expect(result.current[0]).toBe(undefined);
+    });
+
+    it('should be able to set the error message', () => {
+        const { result } = renderHook(() => useDisposableErrorMessage());
+        expect(result.current[0]).toBe(undefined);
+
+        act(() => {
+            result.current[1](newErrorMessage);
+        });
+
+        expect(result.current[0]).toBe(newErrorMessage);
+    });
+
+    describe('disposing a message', () => {
+        it('should dispose an error message after 5 seconds', () => {
+            const { result } = renderHook(() => useDisposableErrorMessage());
+            expect(result.current[0]).toBe(undefined);
+
+            act(() => {
+                result.current[1](newErrorMessage);
+            });
+
+            expect(result.current[0]).toBe(newErrorMessage);
+
+            act(() => {
+                jest.runTimersToTime(5000);
+            });
+
+            expect(result.current[0]).toBe(undefined);
+        });
+
+        it('should dispose an error message after 2 seconds seconds', () => {
+            const disposeTime = 2000;
+
+            const { result } = renderHook(() =>
+                useDisposableErrorMessage(disposeTime),
+            );
+            expect(result.current[0]).toBe(undefined);
+
+            act(() => {
+                result.current[1](newErrorMessage);
+            });
+
+            expect(result.current[0]).toBe(newErrorMessage);
+
+            act(() => {
+                jest.runTimersToTime(disposeTime);
+            });
+
+            expect(result.current[0]).toBe(undefined);
+        });
     });
 });
