3e9861e7c141010386b3cbe81ae08f7ad530c17e
Yannick Fricke <YannickFricke@users.noreply.github.com>
2021-02-21 02:04:04 +0000

    Fixed code style for the 'button' files

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -9,13 +9,15 @@ interface IButtonProps {
     rounded?: boolean;
 }
 
-export function Button(props: React.PropsWithChildren<IButtonProps>): JSX.Element {
+export function Button(
+    props: React.PropsWithChildren<IButtonProps>,
+): JSX.Element {
     const buttonClasses = classNames({
         button: true,
         primary: props.primary ?? false,
         secondary: props.secondary ?? false,
         rounded: props.rounded ?? false,
-    })
+    });
 
-    return <div className={buttonClasses}>{props.children}</div>
+    return <div className={buttonClasses}>{props.children}</div>;
 }
