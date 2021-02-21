33ea53317ddf0d74114c622966442157d121d49f
Yannick Fricke <YannickFricke@users.noreply.github.com>
2021-02-21 02:53:35 +0000

    Added the first routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button } from './ui/Button';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { UserContext } from './context/UserContext';
+import { Login } from './components/menu/auth/Login';
+import { Register } from './components/menu/auth/Register';
 
 export function App() {
-    return <div>App</div>;
+    const userContext = useContext(UserContext);
+    const isUserLoggedIn = userContext?.isLoggedIn() ?? false;
+
+    return (
+        <Switch>
+            {!isUserLoggedIn && (
+                <React.Fragment>
+                    <Route exact path='/'>
+                        <Redirect to='/login' />
+                    </Route>
+                    <Route exact path='/login'>
+                        <Login />
+                    </Route>
+                    <Route exact path='/register'>
+                        <Register />
+                    </Route>
+                </React.Fragment>
+            )}
+            {isUserLoggedIn && (
+                <React.Fragment>
+                    <Route exact path='/'>
+                        <Redirect to='/app' />
+                    </Route>
+                    <Route exact path='/app'>
+                        Welcome to TinDev!
+                    </Route>
+                </React.Fragment>
+            )}
+        </Switch>
+    );
 }
