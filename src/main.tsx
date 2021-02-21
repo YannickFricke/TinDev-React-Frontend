31bdad31060a187e57a0457ac23c783a595a217b
Yannick Fricke <YannickFricke@users.noreply.github.com>
2021-02-21 02:52:43 +0000

    Use the HashRouter and UserContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
+import { RecoilRoot } from 'recoil';
+import { HashRouter } from 'react-router-dom';
+import { UserContextProvider } from './components/menu/provider/UserContextProvider';
 
 render(
-    <App />,
+    <RecoilRoot>
+        <UserContextProvider>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </UserContextProvider>
+    </RecoilRoot>,
     document.getElementById('root'),
-)
+);
